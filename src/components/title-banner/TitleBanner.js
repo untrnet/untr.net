27f47201c9486c456a28fc4c2f84154b90dfcab5
tom.scanlon <tom.scanlon@kingfisher.com>
2018-06-15 16:39:07 +0100

    Implement bulma & banner page

---
+++ src/components/title-banner/TitleBanner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './TitleBanner.css'
+
+const TitleBanner = () => (
+  <section className='hero is-primary is-fullheight has-text-centered'>
+    <div className='hero-body'>
+      <div className='container'>
+        <h1 className='title is-size-1 has-text-black font-mogra'>UNTRNET</h1>
+        <h2 className='subtitle has-text-black'>A network of unique, localised digital art & interactive experiences</h2>
+      </div>
+    </div>
+  </section>
+)
+
+export default TitleBanner
